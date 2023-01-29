370253cd4ded1562e2a25c75f1413d4dd9375494
Aashish Nandakumar <98106129+AashishNandakumar@users.noreply.github.com>
2023-01-29 23:01:16 +0530

    third commit

--- AashishNandakumar_DSA/37_implementCircularQueue.c
+++ AashishNandakumar_DSA/37_implementCircularQueue.c
@@ -1,53 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-int size = 3;
-void insert(int *q, int *rear, int *front, int *count)
+#define size 10
+void insert(int q[], int *rear, int *count)
 {
        int ele;
        printf("Enter element: ");
        scanf("%d", &ele);
 
-       if (*count == size)
-       {
-              size *= 2;
-              int *p;
-              p = (int *)malloc(sizeof(int) * size);
-              int i, j;
-              if (*front == 0)
-              {
-                     for (i = 0, j = 0; j < (size / 2) - 1; i++, j++)
-                     {
-                            p[i] = q[i];
-                     }
-              }
-              else
-              {
-                     for (i = 0, j = *front; j < ((size / 2) - 1); i++, j++)
-                     {
-                            p[i] = q[i];
-                     }
-                     for (j = 0; j < *front; j++, i++)
-                            ;
-                     {
-                            p[i] = q[j];
-                     }
-              }
-              *front = 0;
-              *rear = (size / 2) - 1;
-
-              (*count)++;
-              *rear = ((*rear)++) % size;
-              q[*rear] = ele;
-       }
+       if(*count==size-1)
+              printf("No more elements can be added");
        else
        {
-              (*count)++;
-              *rear = ((*rear)++) % size;
+              (*rear)++;
+              (*rear) %= size;
               q[*rear] = ele;
+              (*count)++;
        }
 }
 
-void delete(int *q, int *front, int *count)
+void delete(int q[], int *front, int *count)
 {
        if (*count == 0)
               printf("no elements to delete");
@@ -61,7 +32,7 @@ void delete(int *q, int *front, int *count)
        }
 }
 
-void display(int *q, int front, int count)
+void display(int q[], int front, int count)
 {
        int i = front;
        while (count--)
@@ -75,8 +46,8 @@ void display(int *q, int front, int count)
 int main()
 {
        int rear = size - 1, front = 0, count = 0;
-       int *q;
-       q = (int *)malloc(sizeof(int) * size);
+       int q[size];
+       
        int choice;
        while (1)
        {
@@ -86,7 +57,7 @@ int main()
               switch (choice)
               {
               case 1:
-                     insert(q, &rear, &front, &count);
+                     insert(q, &rear, &count);
                      break;
               case 2:
                      delete (q, &front, &count);
