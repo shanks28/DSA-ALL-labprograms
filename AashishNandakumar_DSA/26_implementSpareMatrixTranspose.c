75dfa3896310effc24eb2c5965bbb8b08e4aa87c
Aashish Nandakumar <98106129+AashishNandakumar@users.noreply.github.com>
2023-01-29 19:50:27 +0530

    Second commit

--- AashishNandakumar_DSA/26_implementSpareMatrixTranspose.c
+++ AashishNandakumar_DSA/26_implementSpareMatrixTranspose.c
@@ -1,4 +1,3 @@
-// ! NOT CORRECT
 #include <stdio.h>
 
 typedef struct Sparse
@@ -32,31 +31,31 @@ void Accept(S m1[])
     m1[0].value = k - 1;
 }
 
-void transpose(S m1[])
+// does the transpose by traditional method
+void transposeDisplay(S m1[])
 {
-    int temp;
-    temp = m1[0].row;
-    m1[0].row = m1[0].col;
-    m1[0].col = temp;
-    for (int i = 1; i <= m1[0].value; i++)
-    {
-        temp = m1[i].row;
-        m1[i].row = m1[i].col;
-        m1[i].col = temp;
-    }
-}
-
-void Display(S m1[])
-{
-    int k = 1;
+    int k = 1, count = 0;
+    int arr[100][100];
     for (int i = 0; i < m1[0].row; i++)
     {
         for (int j = 0; j < m1[0].col; j++)
         {
             if (m1[k].row == i && m1[k].col == j)
-                printf("%d ", m1[k++].value);
+                arr[i][j] = m1[k++].value;
+                
             else
-                printf("0 ");
+                arr[i][j] = 0;
+                
+        }
+        
+    }
+    
+    for (int i = 0; i < m1[0].col; i++)
+    {
+        
+        for (int j = 0; j < m1[0].row; j++)
+        {
+            printf("%d ", arr[j][i]);
         }
         printf("\n");
     }
@@ -66,8 +65,8 @@ int main()
 {
     S m1[100];
     Accept(m1);
-    transpose(m1);
+    
     printf("Transpose of sparse matrix:\n");
-    Display(m1);
+    transposeDisplay(m1);
     return 0;
 }
